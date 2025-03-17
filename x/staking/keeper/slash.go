09c08f5761afd9f9c6ce7c8deb74730ee0eb44de
nhpd <dmitry.klpv@gmail.com>
2025-03-17 16:53:22 +0400

    improve comment

--- x/staking/keeper/slash.go
+++ x/staking/keeper/slash.go
@@ -172,7 +172,7 @@ func (k Keeper) Slash(ctx context.Context, consAddr sdk.ConsAddress, infractionH
 		hook, isWithBurn := hooks.(types.StakingHooksBeforeValidatorSlashedHasTokensToBurn)
 		if isWithBurn {
 			if err := hook.BeforeValidatorSlashedWithTokensToBurn(ctx, operatorAddress, effectiveFraction, tokensToBurn); err != nil {
-				k.Logger(ctx).Error("failed to call before validator slashed hook", "error", err)
+				k.Logger(ctx).Error("failed to call before validator slashed hook with tokens to burn", "error", err)
 			}
 		} else {
 			if err := hooks.BeforeValidatorSlashed(ctx, operatorAddress, effectiveFraction); err != nil {
